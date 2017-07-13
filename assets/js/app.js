317467c5c06c24454ef484053aceb7660942a7ef
Anna karina Luz <anna.luz.duran@gmail.com>
2017-07-13 16:26:22 -0500

    listo sin parte ninja

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,76 @@
-function getJSON(url){
-        return new Promise(function(resolve,reject){
-          var ajax = new XMLHttpRequest();
-          ajax.open("GET",url);
-          ajax.send();
-          ajax.onreadystatechange = function(){
-            if(ajax.readyState == 4){
-              resolve(JSON.parse(ajax.responseText));
+function getJSON(url) {
+    return new Promise(function (resolve, reject) {
+        var ajax = new XMLHttpRequest();
+        ajax.open("GET", url);
+        ajax.send();
+        ajax.onreadystatechange = function () {
+            if (ajax.readyState == 4) {
+                resolve(JSON.parse(ajax.responseText));
             }
-          }
-        })
-      };
+        }
+    })
+};
 
 getJSON("data/earth-like-results.json")
-.then(function(planetas){
-    planetas.results.forEach(function(planeta){
-    (getJSON(planeta).then(function(datos){
-        console.log(datos)
-    }))
+    .then(function (planetas) {
+        planetas.results.forEach(function (planeta) {
+            (getJSON(planeta).then(function (datos) {
+                crearDatos(datos)
+            }))
+        })
     })
-})
-.then(function(resultado){
-    console.log(resultado)
-});
+
+var crearDatos = function (datos) {
+
+    var nombredatos = datos.pl_name;
+    var descubiertodatos = datos.pl_disc;
+    var orbitadatos = datos.pl_orbper;
+    var contPlanetas = document.getElementById("planetas");
+
+    //crear dinamicamente
+
+    var tarjeta = document.createElement("div");
+    tarjeta.className = "col s12 m6";
+    var card = document.createElement("div");
+    card.className = "card";
+    var cardImage = document.createElement("div");
+    cardImage.className = "card-image";
+    var img = document.createElement("img");
+    img.src = "static/img/planeta1.jpg";
+    var title = document.createElement("span")
+    title.className = "card-title";
+    title.innerText = nombredatos;
+    var cardContent = document.createElement("div");
+    cardContent.className = "card-content";
+    var descubierto = document.createElement("h3");
+    var year = document.createElement("p");
+    year.innerText = descubiertodatos;
+    var orbita = document.createElement("h3");
+
+    var perOrbita = document.createElement("p");
+    perOrbita.innerText = orbitadatos;
 
 
+    cardImage.appendChild(img);
+    cardImage.appendChild(title);
+    cardContent.appendChild(descubierto);
+    cardContent.appendChild(year);
+    cardContent.appendChild(orbita);
+    cardContent.appendChild(perOrbita);
+
+
+    card.appendChild(cardImage);
+    card.appendChild(cardContent);
+    tarjeta.appendChild(card);
+    console.log(tarjeta);
+
+    contPlanetas.appendChild(tarjeta);
+
+    console.log(tarjeta);
+
+};
+
+/*
 var plantilla = '<div class="col s12 m6">'+
                   '<div class="card">'+
                     '<div class="card-image">'+
@@ -41,3 +88,5 @@ var plantilla = '<div class="col s12 m6">'+
                     '</div>'+
                   '</div>'+
                 '</div>';
+                
+                */
