dfb6df6cc1c62a5bf850efea07a41d9a53416ce1
Anna karina Luz <anna.luz.duran@gmail.com>
2017-07-13 14:39:16 -0500

    planetas 1x1

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,9 +12,32 @@ function getJSON(url){
       };
 
 getJSON("data/earth-like-results.json")
-.then(function(mensaje){
-    return(getJSON(mensaje.results[0]))
+.then(function(planetas){
+    planetas.results.forEach(function(planeta){
+    (getJSON(planeta).then(function(datos){
+        console.log(datos)
+    }))
+    })
 })
 .then(function(resultado){
     console.log(resultado)
 });
+
+
+var plantilla = '<div class="col s12 m6">'+
+                  '<div class="card">'+
+                    '<div class="card-image">'+
+                      '<img src="static/img/planeta1.jpg">'+
+                      '<span class="card-title">__nombre__</span>'+
+                    '</div>'+
+                    '<div class="card-content">'+
+                      '<h3>Descubierto en </h3>'+
+                      '<p>__year--</p>'+
+                      '<h3>Periodo de Orbita</h3>'+
+                      '<p>__orbita__</p>'+
+                    '</div>'+
+                   '<div class="card-action">'+
+                      '<a href="#">This is a link</a>'+
+                    '</div>'+
+                  '</div>'+
+                '</div>';
